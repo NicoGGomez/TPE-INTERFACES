c929f1b284cde8190759edd32ed903cdf8e58b68
NicoGGomez <anelecarg@icloud.com>
2025-09-30 12:27:12 -0300

    carrusel en proceso

--- Seguda entrega/scripts/carrusel.js
+++ Seguda entrega/scripts/carrusel.js
@@ -1,22 +1,36 @@
-const carrusel = document.getElementById('contenedor-cards')
+const carrusel = document.querySelector('.cards')
+const card = document.getElementById('card')
 const btnIzq = document.getElementById('btn-izq')
 const btnDer = document.getElementById('btn-der')
 
 let desplazamiento = 0; 
-const anchoCard = 300; 
+const anchoCard = card.offsetWidth; // 206.25 
+
 
 carrusel.style.transform = `translateX(0px)`;
 
+function maxDesplazamiento (){
+    return -(carrusel.scrollWidth - carrusel.parentElement.clientWidth)
+}
+
+
 btnDer.addEventListener('click', () => {
-    desplazamiento -= anchoCard; // se mueve a la izquierda
-    carrusel.style.transform = `translateX(${desplazamiento}px)`;
-    carrusel.style.transition = "transform 0.5s ease";
+    const maxDespla = maxDesplazamiento()
+    const mov = -1 * maxDesplazamiento() / 3; // 206.25 
+
+    if(desplazamiento > maxDespla){ //-780
+        desplazamiento -= mov; // se mueve a la izquierda
+        carrusel.style.transform = `translateX(${desplazamiento}px)`;
+        carrusel.style.transition = "transform 0.5s ease";
+    }
 });
 
 btnIzq.addEventListener('click', () => {
-    if (desplazamiento != 0) {
-        desplazamiento += anchoCard; // se mueve a la derecha
+    const mov = -1 * maxDesplazamiento() / 3; // 206.25 
+    if (desplazamiento != -5.684341886080802e-14 && desplazamiento != 0) {
+        desplazamiento += mov; // se mueve a la derecha
         carrusel.style.transform = `translateX(${desplazamiento}px)`;
         carrusel.style.transition = "transform 0.5s ease"; 
     }
+    alert(desplazamiento)
 });
