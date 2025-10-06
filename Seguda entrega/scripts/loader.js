67cd9b85ae9037b69759dc08363784be4f37c3e2
NicoGGomez <anelecarg@icloud.com>
2025-10-05 23:42:37 -0300

    loader listo y arreglos varios

---
+++ Seguda entrega/scripts/loader.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const loader = document.getElementById("loader");
+    const percentage = document.getElementById("percentage");
+    const progress = document.getElementById("progress");
+    const content = document.getElementById("content");
+    const nave = document.getElementById('nave')
+    
+    let load = 0;
+    const totalDuration = 5000; 
+    const intervalTime = 500; 
+    const increments = totalDuration / intervalTime; // Número de incrementos
+    
+    const interval = setInterval(() => {
+        load += 100 / increments;
+        percentage.innerText = `${Math.round(load)}%`;
+        progress.style.width = `${load}%`;
+        nave.style.left = `${load}%`;
+
+        if (load >= 100) {
+            clearInterval(interval);
+            loader.style.display = "none"; // Oculta el loader
+            content.style.display = "block"; // Muestra el contenido
+        }
+    }, intervalTime);
+});
+
+const corazonButton = document.querySelector('.corazon');
+
+corazonButton.addEventListener('click', () => {
+    corazonButton.classList.toggle('active');
+});
